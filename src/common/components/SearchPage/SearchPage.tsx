9c123dabca1c075ae87ea7990e649074b517a785
Viktoriya Gromova <rockingirl@mail.ru>
2026-07-03 00:16:15 +0500

    SearchPage commit

--- src/common/components/SearchPage/SearchPage.tsx
+++ src/common/components/SearchPage/SearchPage.tsx
@@ -1,12 +1,33 @@
 import s from "./SearchPage.module.css"
 import {SearchForm} from "@/common/components/SearchForm/SearchForm.tsx";
+import {GridComponent} from "@/common/components/GridComponent/GridComponent.tsx";
+import {useSearchMoviesQuery} from "@/features/movies/api/tmdbApi.ts";
+import {useSearchParams} from "react-router";
+import {useState} from "react";
+import {Pagination} from "@/common/components/Pagination/Pagination.tsx";
 
 export const SearchPage = () => {
+    const [params] = useSearchParams();
+    const query = params.get('query') || '';
+    const [currentPage, setCurrentPage] = useState(1)
+    const {data} = useSearchMoviesQuery({query, page: currentPage})
+
+
     return (
-        <>
-            <h1 className={s.title}>Search results</h1>
+        <section className={s.page}>
+            <h1 className={s.title}>Search Results</h1>
             <SearchForm/>
-            <section></section>
-        </>
-)
+            <section className={s.section}>
+                {query.length > 0 &&  <div className={s.header}><h2 className={s.searchTitle}>{`Results for "${query}"`}</h2></div>}
+                {query.length === 0 && <p className={s.emptyMessage}>Enter a movie title to start searching.</p>}
+                {query.length > 0 && data?.results.length === 0 && <p className={s.emptyMessage}>No matches found for "{query}".</p>}
+                <GridComponent movies={data?.results}/>
+                <Pagination
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    pagesCount={data?.total_pages || 1}
+                />
+            </section>
+        </section>
+    )
 }
