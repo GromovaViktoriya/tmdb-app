700b88849de8af5ed98d2ac3ba949d3503879c98
Viktoriya Gromova <rockingirl@mail.ru>
2026-07-09 11:24:55 +0500

    ScrollToTop component fix commit

--- src/common/components/ScrollToTop/ScrollToTop.tsx
+++ src/common/components/ScrollToTop/ScrollToTop.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router";
+import {useLocation, useNavigationType} from "react-router";
 
 export const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { pathname, search } = useLocation();
+    const navType = useNavigationType();
 
+    // "POP" - нажатие кнопок Назад/Вперед в браузере
     useEffect(() => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    }, [pathname]);
+        if (navType !== "POP") {
+            window.scrollTo(0, 0);
+        }
+    }, [navType, pathname, search]);
     return null;
 };
